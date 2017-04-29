7815b10f6c909b0c2322509c85b3900bfb12c1c2
Dhruv Mongia <dhruv.mongia@gmail.com>
2017-04-29 17:41:17 +0530

    without /soil

--- routes/soilRouter.js
+++ routes/soilRouter.js
@@ -2,25 +2,25 @@ var express = require('express');
 
 var bodyParser = require('body-parser');
 var SerialPort = require("serialport");
-var port = new SerialPort("/dev/cu.wchusbserial1410", {
-  baudRate: 9600
-});
+//var port = new SerialPort("/dev/cu.wchusbserial1410", {
+//  baudRate: 9600
+//});
 var x;
-port.on('open', function(){
-  console.log('Serial Port Opend');
-  port.on('data', function(data){
+// port.on('open', function(){
+//   console.log('Serial Port Opend');
+//   port.on('data', function(data){
       
-      x=(data[0]-48);
+//       x=(data[0]-48);
       
-  });
-});
+//   });
+// });
 var soilRouter = express.Router();
 
 soilRouter.use(bodyParser.json());
 
 soilRouter.route('/')
 .get(function (req, res, next) {
-    res.json({"value":x});
+    res.json({"value":0});
 });
 
 
