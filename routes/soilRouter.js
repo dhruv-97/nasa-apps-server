3849f99f19917dfc370b3dd6241f44c7c4acbae6
Dhruv Mongia <dhruv.mongia@gmail.com>
2017-04-30 01:38:10 +0530

    servo motor run through get

--- routes/soilRouter.js
+++ routes/soilRouter.js
@@ -2,6 +2,27 @@ var express = require('express');
 
 var bodyParser = require('body-parser');
 var soils= require('../models/soil');
+var five = require("johnny-five"),board,myServo;
+
+
+function runMotor(){
+	
+	board = new five.Board({port:"/dev/cu.wchusbserial1420",sigint: false,repl: false,lock:false});
+    board.on("ready", function() {
+
+	  myServo = new five.Servo(9);	  
+	  myServo.sweep();
+	  this.wait(5000, function(){
+
+	    myServo.stop();
+	    myServo.center();
+
+
+	  });
+  
+	});
+}
+
 var soilRouter = express.Router();
 
 soilRouter.use(bodyParser.json());
@@ -13,6 +34,10 @@ soilRouter.route('/')
         res.json(soil);
         });
 });
-
+soilRouter.route('/water')
+.get(function (req, res, next) {
+	runMotor();
+	res.send("Done");
+});
 
 module.exports=soilRouter;
